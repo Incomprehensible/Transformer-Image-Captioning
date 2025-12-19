72fc3d98c63bd74b37521182a7e07d09b7f52a73
Nadezhda Varzonova <varzonova.nadezhda@gmail.com>
2025-12-19 03:03:58 +0100

    feat: Implemented transformer with 2 patch embedders and encoder.

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ USE_EVAL_DATASET=True
 EVAL_DATASET_INFO_PATH= 'imageinwords/datasets'
 EVAL_DATASET_SPLIT='IIW-400'
 
+NUM_INPUT_CHANNELS = 3
+
 IMG_HEIGHT = 224
 IMG_WIDTH = 224
 PATCH_SIZE = 16 #112 #16
@@ -14,4 +16,30 @@ PATCH_SIZE = 16 #112 #16
 
 TEXT_VOCAB_SIZE = 1000
 TEXT_EMBEDDING_DIM = 300
+TEXT_MAX_CAPTION_LEN = 500 # needs to be adjusted based on dataset
 IMG_EMBEDDING_DIM = 300
+
+USE_CONV_IMG_EMBEDDING = False
+
+ENCODER_NUM_BLOCKS = 10
+ENCODER_NUM_HEADS = 10
+ENCODER_DROPOUT_PROB = 0.1
+ENCODER_HIDDEN_DIM = IMG_EMBEDDING_DIM * 4
+
+# Params from CPTR paper:
+# BATCH_SIZE         = 1              #(1)
+
+# IMAGE_SIZE         = 384            #(2)
+# IN_CHANNELS        = 3              #(3)
+
+# SEQ_LENGTH         = 30             #(4)
+# VOCAB_SIZE         = 10000          #(5)
+
+# EMBED_DIM          = 768            #(6)
+# PATCH_SIZE         = 16             #(7)
+# NUM_PATCHES        = (IMAGE_SIZE//PATCH_SIZE) ** 2  #(8)
+# NUM_ENCODER_BLOCKS = 12             #(9)
+# NUM_DECODER_BLOCKS = 4              #(10)
+# NUM_HEADS          = 12             #(11)
+# HIDDEN_DIM         = EMBED_DIM * 4  #(12)
+# DROP_PROB          = 0.1            #(13)
